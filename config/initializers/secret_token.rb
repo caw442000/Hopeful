7af26492e8b93e011770d2276fbf3bb9dfce996b
Cedric Winbush <cawinbushjr@gmail.com>
2013-01-10 14:45:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hopeful::Application.config.secret_token = 'c09e436ecd1c73174d1a4d9482bc947519aa5ec2dbd294e172ac0c5ebe3c6189158a75cf8644e6acd3c4540c875461df491c1b74eadc821ce2fc410145a6e377'
